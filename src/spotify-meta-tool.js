dc8a1f290a06861df84c981c5947d0eb963ca9b4
Isaac Yong <isaac.yongty@gmail.com>
2019-07-07 04:10:01 +0800

    Modified to use hbs

--- src/spotify-meta-tool.js
+++ src/spotify-meta-tool.js
@@ -10,58 +10,6 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
-
-app.use(express.static(publicDirectoryPath))
-
-// Configure express to use hbs
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Home',
-    name: 'Isaac Yong'
-  })
-})
-app.get('/help', (req, res) => {
-  res.render('help', {
-    title: 'Help',
-    name: 'Isaac Yong'
-  })
-})
-
-app.get('/bpm', (req, res) => {
-  res.render('bpm', {
-    title: 'BPM Tool',
-    name: 'Isaac Yong'
-  })
-})
-
-app.get('/progression-generator', (req, res) => {
-  res.render('progression-generator', {
-    title: 'Progression Generator',
-    name: 'Isaac Yong'
-  })
-})
-
-app.get('/spotify-meta-tool', (req, res) => {
-  res.render('spotify-meta-tool', {
-    title: 'Spotify Meta Tool',
-    name: 'Isaac Yong'
-  })
-})
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About',
-    name: 'Isaac Yong'
-  })
-})
-
-
 const assignPitchClass = myFunctions.assignPitchClass
 const convertDurationToMinSec = myFunctions.convertDurationToMinSec
 const convertTime = myFunctions.convertTime
@@ -151,9 +99,7 @@ spotifyApi.clientCredentialsGrant().then(
   }
 )
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`)
-})
+
 
 
   
