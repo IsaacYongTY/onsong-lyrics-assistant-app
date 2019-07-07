fe9179d23ba86238493a996ef9d0b8351d5bc411
Isaac Yong <isaac.yongty@gmail.com>
2019-07-08 02:44:36 +0800

    test

--- src/utils/web-scraper.js
+++ src/utils/web-scraper.js
@@ -18,7 +18,7 @@ const webScraper = (url,callback) => {
         url: url,
         proxy: process.env.QUOTAGUARDSTATIC_URL,
         headers: {
-            'User-Agent': 'node.js'
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
         }
     }
     request(option, (error,response,html) => {
